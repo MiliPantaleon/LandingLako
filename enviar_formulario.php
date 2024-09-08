7dd536620e4cd1a329df7549b0b0be6968538ca2
MiliPantaleon <mmpantaleon@gmail.com>
2024-09-08 19:23:42 -0300

    commit

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -6,13 +6,13 @@ ini_set('display_errors', 1);
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$ubicacion = $_POST['ubicacion'];
+$mensaje_usuario = $_POST['mensaje']; // El campo mensaje que contiene la provincia y ciudad
 
 // Construir el mensaje
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su email es: " . $email . ",\r\n";
 $mensaje .= "Su teléfono es: " . $phone . ",\r\n";
-$mensaje .= "Vive en: " . $ubicacion . ",\r\n";
+$mensaje .= "Vive en: " . $mensaje_usuario . ",\r\n";
 
 // Definir mail de recepción y asunto
 $destinatario = "estudio@lako.com.ar";
@@ -21,12 +21,14 @@ $asunto = "Hola quiero trabajar con LAKO stands";
 // Construir las cabeceras
 $headers = "From: " . $email . "\r\n" .
            "Reply-To: " . $email . "\r\n" .
-           "X-Mailer: PHP/" . phpversion();
+           "X-Mailer: PHP/" . phpversion() . "\r\n" .
+           "MIME-Version: 1.0\r\n" .
+           "Content-Type: text/plain; charset=utf-8\r\n";
 
 // Enviar el correo
 if (mail($destinatario, $asunto, $mensaje, $headers)) {
     // Redirigir después de enviar el formulario
-    header("Location: stands.estudiolako.com");
+    header("Location: https://stands.estudiolako.com"); // Asegúrate de que la URL sea correcta
     exit();  // Asegúrate de detener el script después de la redirección
 } else {
     echo "Error al enviar el correo.";
